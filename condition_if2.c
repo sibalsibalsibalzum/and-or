cbd42ecc5d0dd69b5c39f1bb5fdfecc3ac57c6cd
31127 <>
2023-04-06 04:59:08 +0000

    Initial commit

---
+++ condition_if2.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+int main() {
+  if (5 > 3) {
+    printf("A\n");
+    printf("B\n");
+  }
+  printf("C\n");
+
+  if (5 < 3) {
+    printf("D\n");
+    printf("E\n");
+  }
+  printf("F\n");
+}
+*/
